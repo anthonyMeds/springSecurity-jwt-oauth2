fe663ae64299a9936922a5c75aec2a85608243d5
anthonyMeds <anthony.eduardo@hotmail.com>
2024-07-08 17:56:35 -0300

    feat : repository

---
+++ src/main/java/project/springsecurity/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package project.springsecurity.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import project.springsecurity.entity.User;
+
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+
+}
